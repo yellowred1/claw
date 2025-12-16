9dd6ee884980c994eb423a4591a3e0ddb6c67723
yellowred1 <141902045+yellowred1@users.noreply.github.com>
2025-12-16 21:26:31 +0800

    Update app.py

--- scripts/app.py
+++ scripts/app.py
@@ -13,6 +13,7 @@ co.set_argument("--window-size=1280,720")
 browser = Chromium(co)
 page= browser.latest_tab
 page.get('https://eu-central-1.run.claw.cloud/signin')
+print('---'*50)
 print(page.ele('.css-1xigfyl').text)
 time.sleep(5)
 # 2. 点击 GitHub
@@ -20,3 +21,4 @@ page.ele('.chakra-button css-1ggp06u').click()
 time.sleep(5)
 print('---'*50)
 print(page.ele('.authentication ').text)
+print('---'*50)
