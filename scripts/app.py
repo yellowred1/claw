931a8398479143e2db1307fd3638fd28557bcab3
yellowred1 <141902045+yellowred1@users.noreply.github.com>
2025-12-16 21:19:27 +0800

    Update app.py

--- scripts/app.py
+++ scripts/app.py
@@ -6,12 +6,9 @@ co = ChromiumOptions()
 co.headless(True)
 co.set_argument("--headless=new")
 co.set_argument("--blink-settings=imagesEnabled=true")
-# 额外添加Linux下的浏览器优化参数
 co.set_argument("--no-sandbox")
 co.set_argument("--disable-dev-shm-usage")
-# 可选：设置窗口大小（非无头模式下更友好）
 co.set_argument("--window-size=1280,720")
-# co.set_browser_path('/opt/google/chrome/google-chrome')
 
 browser = Chromium(co)
 page= browser.latest_tab
